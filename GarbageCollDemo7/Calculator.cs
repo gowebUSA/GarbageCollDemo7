02dcc30c931503c0209b7d5039220ca192b57e98
Richard Go <60155239+gowebUSA@users.noreply.github.com>
2020-03-13 23:38:39 -0700

    Add project files.

---
+++ GarbageCollDemo7/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollDemo7
+{
+    class Calculator : IDisposable
+    {
+        private bool disposed = false;
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+            this.Dispose();
+        }
+        public void Dispose()               //added on page 332.
+        {
+            lock (this)
+            {
+                if (!disposed)
+                {
+                    Console.WriteLine("Calculator being disposed");
+                }
+                this.disposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+        /*
+        public void Dispose()                       //commented on page 332.
+        {
+            if (!this.disposed)                     //added on page 331.
+            {
+                Console.WriteLine("Calculator being disposed");
+            }
+            this.disposed = true;
+            GC.SuppressFinalize(this);
+        }
+        */
+    }
+}
---
+++ GarbageCollDemo7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollDemo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator calculator = new Calculator();             //commented out on page 330.
+            //Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");
+            using (Calculator calculator = new Calculator())            //added on page 330.
+            {
+                //Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");   //commented out on page 333.
+                Console.WriteLine($"120 / 0 = {calculator.Divide(120, 0)}");        //added out on page 333.
+            }
+            Console.WriteLine("Program finishing");
+        }
+    }
+}
